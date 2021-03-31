d47f5346b89cc1f92d9fe96787bc31e0a0eef339
Jan Georg Henriksen <jangeorg85@gmail.com>
2021-03-31 10:06:09 +0200

    converted hex  to rgb in h3

--- script.js
+++ script.js
@@ -3,6 +3,26 @@ var color1=document.querySelector(".color1");
 var color2=document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
+function hexToRGB(h) {
+    let r = 0, g = 0, b = 0;
+  
+    // 3 digits
+    if (h.length == 4) {
+      r = "0x" + h[1] + h[1];
+      g = "0x" + h[2] + h[2];
+      b = "0x" + h[3] + h[3];
+  
+    // 6 digits - convert to hex
+    } else if (h.length == 7) {
+      r = "0x" + h[1] + h[2];
+      g = "0x" + h[3] + h[4];
+      b = "0x" + h[5] + h[6];
+    }
+    
+    // + converts from string back to number
+    return `rgb (${+r}, ${+g}, ${+b})`;
+}
+
 function changeGradientColor() {
     body.style.background = 
     "linear-gradient(to right, " 
@@ -11,7 +31,8 @@ function changeGradientColor() {
     + color2.value
     + ")"; 
 
-    css.textContent = body.style.background;
+    /* css.textContent = `Left: ${color1.value} Right: ${color2.value}`; */
+    css.textContent = `Left: ${hexToRGB(color1.value)} \u00A0 \u00A0 Right: ${hexToRGB(color2.value)}`;
 };
 
 color1.addEventListener("input", changeGradientColor);
